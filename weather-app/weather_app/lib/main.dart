97a1f79ca886f4a29e524998249123e1b457d9fb
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-01 11:43:56 +0200

    Create weather project

---
+++ weather-app/weather_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(WeatherApp());
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Weather"),
+        ),
+      ),
+    );
+  }
+}
