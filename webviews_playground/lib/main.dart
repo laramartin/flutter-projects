060ce9e691810cc159ff7214dea76c02ea03de77
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-01 10:46:44 +0200

    WebView usage sample

--- webviews_playground/lib/main.dart
+++ webviews_playground/lib/main.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(WebViewDemo());
 
-class MyApp extends StatelessWidget {
-
+class WebViewDemo extends StatelessWidget {
   WebViewController _controller;
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -22,30 +17,54 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("WebViews playground"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () async {
+                await _controller.reload();
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () async {
+                if (await _controller.canGoBack()) {
+                  print(Text("can go back!"));
+                  _controller.goBack();
+                } else {
+                  print(Text("cannot go back"));
+                  // TODO: disable button
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () async {
+                if (await _controller.canGoForward()) {
+                  _controller.goForward();
+                } else {
+                  // TODO: disable button
+                }
+              },
+            )
+          ],
         ),
-        body:
-//        Container(
-//          color: Colors.orange,
-//        ),
-            WillPopScope(
-          child: WebView(
-            initialUrl: "https://www.github.com",
-            javaScriptMode: JavaScriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller = webViewController;
-            },
-          ),
-          onWillPop: () async {
-            var canGoBack = await _controller.canGoBack();
-            if (canGoBack) {
-              _controller.goBack();
-              return false;
-            } else {
-              return true;
-            }
+        body: WebView(
+          initialUrl: "https://www.flutter.dev/",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller = webViewController;
+          },
+          onPageFinished: (url) {
+            // use the URL
+          },
+          navigationDelegate: (request) {
+            bool isHost = request.url.startsWith("https://flutter.dev");
+            if (isHost)
+              return NavigationDecision.navigate;
+            else
+              return NavigationDecision.prevent;
           },
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
