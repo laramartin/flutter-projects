dc8e1a21f2de79a17b295d947b9ce1abd99a217d
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-02 20:48:30 +0200

    Extract WeatherApp

--- weather-app/weather_app/lib/main.dart
+++ weather-app/weather_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:weather_app/ui/currentweather/current_weather_header.dart';
-import 'package:weather_app/ui/currentweather/current_weather_content.dart';
+import 'package:weather_app/weather_app.dart';
 import 'data/current_weather_service.dart';
 
 void main() {
@@ -9,46 +8,4 @@ void main() {
     model: CurrentWeatherService(),
     child: WeatherApp(),
   ));
-}
-
-class WeatherApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    ScopedModel.of<CurrentWeatherService>(context).fetchWeather();
-
-    return MaterialApp(
-      title: 'Weather',
-      theme: ThemeData(textTheme: Typography.blackMountainView,
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Weather"),
-        ),
-        body: ScopedModelDescendant<CurrentWeatherService>(
-            builder: (context, child, model) {
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: <Widget>[
-                CurrentWeatherHeader(
-                  city: model.getCurrentWeatherCity(),
-                  date: "Sun, Jun 2",
-                  hour: "15:51",
-                ),
-                CurrentWeatherContent(
-                  iconAsset: model.getCurrentWeatherIcon(),
-                  temperature: model.getCurrentTemperature(),
-                  description: model.getCurrentWeatherDescription(),
-                  pressure: model.getCurrentWeatherPressure(),
-                  humidity: model.getCurrentWeatherHumidity(),
-                  windSpeed: model.getCurrentWeatherWindSpeed(),
-                ),
-              ],
-            ),
-          );
-        }),
-      ),
-    );
-  }
 }
---
+++ weather-app/weather_app/lib/weather_app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:weather_app/ui/currentweather/current_weather_header.dart';
+import 'package:weather_app/ui/currentweather/current_weather_content.dart';
+import 'data/current_weather_service.dart';
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScopedModel.of<CurrentWeatherService>(context).fetchWeather();
+
+    return MaterialApp(
+      title: 'Weather',
+      theme: ThemeData(
+        textTheme: Typography.blackMountainView,
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Weather"),
+        ),
+        body: ScopedModelDescendant<CurrentWeatherService>(
+            builder: (context, child, model) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                CurrentWeatherHeader(
+                  city: model.getCurrentWeatherCity(),
+                  date: "Sun, Jun 2",
+                  hour: "15:51",
+                ),
+                CurrentWeatherContent(
+                  iconAsset: model.getCurrentWeatherIcon(),
+                  temperature: model.getCurrentTemperature(),
+                  description: model.getCurrentWeatherDescription(),
+                  pressure: model.getCurrentWeatherPressure(),
+                  humidity: model.getCurrentWeatherHumidity(),
+                  windSpeed: model.getCurrentWeatherWindSpeed(),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
