509aa76157ec7d817f19a37fd1f224400511ca6d
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-02 15:12:32 +0200

    high level method to get weather asset, try them out

--- weather-app/weather_app/lib/main.dart
+++ weather-app/weather_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'data/current_weather_service.dart';
+import 'utils/icons_utils.dart';
 
 void main() {
   return runApp(ScopedModel<CurrentWeatherService>(
@@ -25,11 +26,14 @@ class WeatherApp extends StatelessWidget {
         ),
         body: ScopedModelDescendant<CurrentWeatherService>(
             builder: (context, child, model) {
-
+          var asset = getWeatherIcon(model.getCurrentWeatherIcon());
           return Column(
             children: <Widget>[
               Text("${model.getCurrentTemperature()}"),
-              Image.network("http://openweathermap.org/img/w/${model.getCurrentWeatherIcon()}.png"),
+              Image.network(
+                  'http://openweathermap.org/img/w/${model.getCurrentWeatherIcon()}.png'),
+              Image.asset(asset ?? 'weather-unknown'),
+              Text("icon: ${model.getCurrentWeatherIcon()}"),
               Text("${model.getCurrentWeatherDescription()}")
             ],
           );
