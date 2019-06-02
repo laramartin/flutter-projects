cc88be2a09e22392eee8d19aadf874478466fc7f
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-02 17:51:51 +0200

    Define Material Type Scale

---
+++ weather-app/weather_app/lib/utils/styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TypeScale {
+
+  static const headline1 = TextStyle(fontFamily: 'Roboto', fontSize: 96.0, letterSpacing: -1.5);
+  static const headline2 = TextStyle(fontFamily: 'Roboto', fontSize: 60.0, letterSpacing: -0.5);
+  static const headline3 = TextStyle(fontFamily: 'Roboto', fontSize: 48.0, letterSpacing: 0);
+  static const headline4 = TextStyle(fontFamily: 'Roboto', fontSize: 34.0, letterSpacing: 0.25);
+  static const headline5 = TextStyle(fontFamily: 'Roboto', fontSize: 24.0, letterSpacing: 0);
+  static const headline6 = TextStyle(fontFamily: 'Roboto', fontSize: 20.0, letterSpacing: 0.15);
+  static const subtitle1 = TextStyle(fontFamily: 'Roboto', fontSize: 16.0, letterSpacing: 0.15);
+  static const subtitle2 = TextStyle(fontFamily: 'Roboto', fontSize: 14.0, letterSpacing: 0.1);
+  static const body1 = TextStyle(fontFamily: 'Roboto', fontSize: 16.0, letterSpacing: 0.5);
+  static const body2 = TextStyle(fontFamily: 'Roboto', fontSize: 14.0, letterSpacing: 0.25);
+  static const button = TextStyle(fontFamily: 'Roboto', fontSize: 14.0, letterSpacing: 1.25);
+  static const caption = TextStyle(fontFamily: 'Roboto', fontSize: 12.0, letterSpacing: 0.4);
+  static const overline = TextStyle(fontFamily: 'Roboto', fontSize: 10.0, letterSpacing: 1.5);
+}
+
+
