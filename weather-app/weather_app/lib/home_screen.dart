222414729451ea15949b851fcb64cc7b114878bc
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-15 20:12:10 +0200

    Update forecasts UI with its model

--- weather-app/weather_app/lib/home_screen.dart
+++ weather-app/weather_app/lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:weather_app/data/forecast_weather_model.dart';
 import 'package:weather_app/ui/currentweather/current_weather_content.dart';
 import 'package:weather_app/ui/currentweather/current_weather_header.dart';
 import 'package:weather_app/ui/forecast/forecast_item.dart';
@@ -14,38 +15,51 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Weather"),
-      ),
-      body: ScopedModelDescendant<CurrentWeatherModel>(
-          builder: (context, child, model) {
-            var forecastItem = ForecastItem("Sun", "01n", 3, 6);
-            var forecast = [forecastItem, forecastItem, forecastItem, forecastItem, forecastItem];
+    var currentWeatherModel =
+        ScopedModel.of<CurrentWeatherModel>(context, rebuildOnChange: true);
+    var forecastWeatherModel =
+        ScopedModel.of<ForecastWeatherModel>(context, rebuildOnChange: true);
 
-        return Padding(
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Weather"),
+        ),
+        body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: <Widget>[
               CurrentWeatherHeader(
-                city: model.getCurrentWeatherCity(),
+                city: currentWeatherModel.getCurrentWeatherCity(),
                 // TODO: use real data formatted
                 date: "Sun, Jun 2",
                 hour: "15:51",
               ),
               CurrentWeatherContent(
-                iconAsset: model.getCurrentWeatherIcon(),
-                temperature: model.getCurrentTemperature(),
-                description: model.getCurrentWeatherDescription(),
-                pressure: model.getCurrentWeatherPressure(),
-                humidity: model.getCurrentWeatherHumidity(),
-                windSpeed: model.getCurrentWeatherWindSpeed(),
+                iconAsset: currentWeatherModel.getCurrentWeatherIcon(),
+                temperature: currentWeatherModel.getCurrentTemperature(),
+                description: currentWeatherModel.getCurrentWeatherDescription(),
+                pressure: currentWeatherModel.getCurrentWeatherPressure(),
+                humidity: currentWeatherModel.getCurrentWeatherHumidity(),
+                windSpeed: currentWeatherModel.getCurrentWeatherWindSpeed(),
               ),
-              ForecastWidget(forecast),
+              ForecastWidget(mapForecastToForecastItems(forecastWeatherModel)),
             ],
           ),
-        );
-      }),
-    );
+        ));
   }
+}
+
+List<ForecastItem> mapForecastToForecastItems(ForecastWeatherModel model) {
+  var forecasts = model.getForecasts();
+  var forecastItems = List<ForecastItem>();
+  forecasts.forEach((forecastDay) {
+    ForecastItem item = ForecastItem(
+      day: "Sun",
+      minTemperature: model.getForecastMinTemperature(forecastDay),
+      maxTemperature: model.getForecastMaxTemperature(forecastDay),
+      iconId: model.getForecastIcon(forecastDay),
+    );
+    forecastItems.add(item);
+  });
+  return forecastItems;
 }
