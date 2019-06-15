ac2c4ea2c11a087eba05f091e48a098d29462ed7
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-15 20:11:55 +0200

    Add methods to ForecastWeatherModel

--- weather-app/weather_app/lib/data/forecast_weather_model.dart
+++ weather-app/weather_app/lib/data/forecast_weather_model.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:scoped_model/scoped_model.dart';
 import '../keys.dart';
+import 'model/forecastweather/forecast_day.dart';
 import 'model/forecastweather/forecast_weather_response.dart';
 
-class
-ForecastWeatherModel extends Model {
+class ForecastWeatherModel extends Model {
   ForecastWeatherResponse forecast;
 
   fetchForecast() async {
@@ -15,11 +15,22 @@ ForecastWeatherModel extends Model {
     var response = ForecastWeatherResponse.fromJson(json);
 
     var daySample = response.forecasts[0];
-    print("forecast day ${daySample.timestamp}. Min temp: ${daySample.temperature.minTemperature}. "
+    print(
+        "forecast day ${daySample.timestamp}. Min temp: ${daySample.temperature.minTemperature}. "
         "Max temp: ${daySample.temperature.maxTemperature}");
 
     forecast = response;
 
     notifyListeners();
   }
+
+  List<ForecastDay> getForecasts() => forecast?.forecasts;
+
+  int getForecastTimeStamp(ForecastDay forecastDay) => forecastDay.timestamp;
+
+  int getForecastMinTemperature(ForecastDay forecastDay) => forecastDay.temperature.minTemperature.round();
+
+  int getForecastMaxTemperature(ForecastDay forecastDay) => forecastDay.temperature.maxTemperature.round();
+
+  String getForecastIcon(ForecastDay forecastDay) => forecastDay.weather[0].iconId;
 }
