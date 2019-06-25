8442fa533e00c96942fe1e522728204524410051
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-25 19:17:48 +0200

    Remove container, add some logs

--- webviews_playground/lib/main.dart
+++ webviews_playground/lib/main.dart
@@ -6,86 +6,78 @@ void main() => runApp(WebViewDemo());
 
 class WebViewDemo extends StatelessWidget {
   WebViewController _controller;
+  String host = "https://www.flutter.dev/";
 
   @override
   Widget build(BuildContext context) {
     var _channel = JavascriptChannel(
-      name: "LaraDemo",
-      onMessageReceived: (message) {
-        print(message.message);
-      }
-    );
+        name: "LaraDemo",
+        onMessageReceived: (message) {
+          print(message.message);
+        });
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("WebViews playground"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () async {
-                await _controller.reload();
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () async {
-                if (await _controller.canGoBack()) {
-                  print(Text("can go back!"));
-                  _controller.goBack();
-                } else {
-                  print(Text("cannot go back"));
-                  // TODO: disable button
-                }
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.arrow_forward),
-              onPressed: () async {
-                if (await _controller.canGoForward()) {
-                  _controller.goForward();
-                } else {
-                  // TODO: disable button
-                }
-              },
-            )
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
         ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: 400.0,
-              child: WebView(
-                initialUrl: "https://www.flutter.dev/",
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (WebViewController webViewController) {
-                  _controller = webViewController;
-                },
-                onPageFinished: (url) {
-                  // use the URL
-                },
-                navigationDelegate: (request) {
-                  bool isHost = request.url.startsWith("https://flutter.dev");
-                  if (isHost)
-                    return NavigationDecision.navigate;
-                  else
-                    return NavigationDecision.prevent;
-                },
-              ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("WebViews playground"),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () async {
+                    await _controller.reload();
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () async {
+                    if (await _controller.canGoBack()) {
+                      print(Text("can go back!"));
+                      _controller.goBack();
+                    } else {
+                      print(Text("cannot go back"));
+                      // TODO: disable button
+                    }
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () async {
+                    if (await _controller.canGoForward()) {
+                      _controller.goForward();
+                    } else {
+                      // TODO: disable button
+                    }
+                  },
+                )
+              ],
             ),
-            Container(
-              height: 100.0,
-              child: WebView(
-                initialUrl: "http://192.168.0.55:8000",
-                javascriptChannels: {_channel},
+            body: Column(children: <Widget>[
+              Container(
+                height: 400.0,
+                child: WebView(
+                  initialUrl: host,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (WebViewController webViewController) {
+                    _controller = webViewController;
+                  },
+                  onPageFinished: (url) {
+                    // use the URL
+                  },
+                  navigationDelegate: (request) {
+                    bool isHost = request.url.startsWith(host);
+                    if (isHost) {
+                      print("can navigate. Request url: ${request.url}");
+                      return NavigationDecision.navigate;
+                    } else {
+                      print("cannot navigate. Request url: ${request.url}");
+                      return NavigationDecision.prevent;
+                    }
+                  },
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            ])));
   }
 }
