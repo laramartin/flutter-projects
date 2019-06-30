160a003eb0ae0d0fe345eea12a56a4c676de930b
Lara Martin <lara.martin.carretero@gmail.com>
2019-06-30 19:23:54 +0200

    Show different screens for different bottom nav tabs

--- weather-app/weather_app/lib/home_screen.dart
+++ weather-app/weather_app/lib/home_screen.dart
@@ -46,37 +46,8 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: Text("Weather"),
         ),
-        body: Padding(
-          padding:
-              const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 8),
-          child: Column(
-            children: <Widget>[
-              CurrentWeatherHeader(
-                city: currentWeatherModel.getCurrentWeatherCity(),
-                date: getCurrentTimeDate(
-                    currentWeatherModel.getCurrentWeatherTimestamp()),
-                hour: getCurrentTimeHours(
-                    currentWeatherModel.getCurrentWeatherTimestamp()),
-              ),
-              Flexible(
-                flex: 4,
-                child: CurrentWeatherContent(
-                  iconAsset: currentWeatherModel.getCurrentWeatherIcon(),
-                  temperature: currentWeatherModel.getCurrentTemperature(),
-                  description:
-                      currentWeatherModel.getCurrentWeatherDescription(),
-                  pressure: currentWeatherModel.getCurrentWeatherPressure(),
-                  humidity: currentWeatherModel.getCurrentWeatherHumidity(),
-                  windSpeed: currentWeatherModel.getCurrentWeatherWindSpeed(),
-                ),
-              ),
-              Flexible(
-                  flex: 2,
-                  child: ForecastWidget(
-                      mapForecastToForecastItems(forecastWeatherModel))),
-            ],
-          ),
-        ),
+        body: _selectedScreen(
+            currentWeatherModel, forecastWeatherModel, currentIndex),
         bottomNavigationBar: BottomNavigationBar(
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
@@ -92,6 +63,84 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       );
     }
+  }
+
+  Widget _selectedScreen(CurrentWeatherModel currentWeatherModel,
+      ForecastWeatherModel forecastWeatherModel, int index) {
+    switch (index) {
+      case 0:
+        return FavoriteScreen(
+            currentWeatherModel: currentWeatherModel,
+            forecastWeatherModel: forecastWeatherModel);
+      case 1:
+        return CitiesScreen();
+      case 2:
+        return PreferencesScreen();
+    }
+    return Container(); // FIX ME: UGLY UGH
+  }
+}
+
+// TODO: move outside
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({
+    Key key,
+    @required this.currentWeatherModel,
+    @required this.forecastWeatherModel,
+  }) : super(key: key);
+
+  final CurrentWeatherModel currentWeatherModel;
+  final ForecastWeatherModel forecastWeatherModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 8),
+      child: Column(
+        children: <Widget>[
+          CurrentWeatherHeader(
+            city: currentWeatherModel.getCurrentWeatherCity(),
+            date: getCurrentTimeDate(
+                currentWeatherModel.getCurrentWeatherTimestamp()),
+            hour: getCurrentTimeHours(
+                currentWeatherModel.getCurrentWeatherTimestamp()),
+          ),
+          Flexible(
+            flex: 4,
+            child: CurrentWeatherContent(
+              iconAsset: currentWeatherModel.getCurrentWeatherIcon(),
+              temperature: currentWeatherModel.getCurrentTemperature(),
+              description: currentWeatherModel.getCurrentWeatherDescription(),
+              pressure: currentWeatherModel.getCurrentWeatherPressure(),
+              humidity: currentWeatherModel.getCurrentWeatherHumidity(),
+              windSpeed: currentWeatherModel.getCurrentWeatherWindSpeed(),
+            ),
+          ),
+          Flexible(
+              flex: 2,
+              child: ForecastWidget(
+                  mapForecastToForecastItems(forecastWeatherModel))),
+        ],
+      ),
+    );
+  }
+}
+
+class CitiesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlueAccent,
+    );
+  }
+}
+
+class PreferencesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink,
+    );
   }
 }
 
